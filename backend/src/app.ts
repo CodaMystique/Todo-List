6aa4c1b41894879a5ef868a784df52bd872242a4
CodaMystique <codamystique@gmail.com>
2024-06-11 11:14:50 +0530

    some changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,17 +2,12 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import todosRoutes from "./routes/todos.routes.ts";
-import connectToMongoDB from "./db/connectToMongoDB.ts";
 
 dotenv.config();
 
 const app: Express = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use(todosRoutes);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import connectToMongoDB from "./db/connectToMongoDB";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectToMongoDB();
